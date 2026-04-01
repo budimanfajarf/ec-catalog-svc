32d6fc9c1b67f4ea3d115d84258293e488a5a5b7
Budi☕ <budimanfajarf@gmail.com>
2026-04-01 15:01:09 +0700

    feat: add hot reload & hello endpoint

--- src/main/java/day/budi/catalog/HomeController.java
+++ src/main/java/day/budi/catalog/HomeController.java
@@ -1,13 +1,22 @@
 package day.budi.catalog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
         return "index.html";
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }
