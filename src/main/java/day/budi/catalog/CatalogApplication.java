5ead1d61086bd1e344590ae84058ea0afac5a944
Budi☕ <budimanfajarf@gmail.com>
2026-04-02 11:05:43 +0700

    chore: setup db

--- src/main/java/day/budi/catalog/CatalogApplication.java
+++ src/main/java/day/budi/catalog/CatalogApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class CatalogApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(CatalogApplication.class, args);
+		SpringApplication.run(CatalogApplication.class, args);
 
 		// // https://youtu.be/gJrjgg1KVL4?si=bwUFK2pv0wRp8WkL
 		// // it's an example of a Open/Closed Principle
@@ -16,9 +16,9 @@ public class CatalogApplication {
 		// OrderService orderService = new OrderService(new PaypalService());
 		// orderService.placeOrder("ORDER123", 100000);
 
-		// Spring IoC Container will manage the dependencies injection for us
-		var orderService = context.getBean(OrderService.class);
-		orderService.placeOrder("ORDER123", 100000);
+		// // Spring IoC Container will manage the dependencies injection for us
+		// var orderService = context.getBean(OrderService.class);
+		// orderService.placeOrder("ORDER123", 100000);
 	}
 
 }
